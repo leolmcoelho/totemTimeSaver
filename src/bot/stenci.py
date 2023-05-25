cc648d7a23e54d4ba05084d3b5d3c5b74f40bc22
CoelhoBranco <leonardo_leal15@outlook.com>
2023-05-25 15:52:20 -0300

    atualização stenci

--- src/bot/stenci.py
+++ src/bot/stenci.py
@@ -28,7 +28,7 @@ os.environ['WDM_LOG'] = str(log.NOTSET)
 logging = get_logger()
 
 
-class Stenci:
+class Stenci(Interation):
 
     def __init__(self, user, password, teste=False):
 
@@ -48,14 +48,14 @@ class Stenci:
         
         if not teste:
             self.driver.minimize_window()
-
-        self.i = Interation(self.driver)
+        
+        super().__init__(self.driver)
 
         self.driver.get("https://stenci.app")
         
         
         try:
-            self.i.locacated('//*[@id="app"]/section/aside/nav/a[2]/div', 3)
+            self.locacated('//*[@id="app"]/section/aside/nav/a[2]/div', 3)
 
         except:
             if self.logado() == False:
@@ -65,7 +65,7 @@ class Stenci:
         
     def logado(self):
         try:
-            if self.i.locacated('//*[@id="username"]', time=1):
+            if self.locacated('//*[@id="username"]', time=1):
                 js ='document.querySelector("body > div.login-page > div.login-form > form > div.form-body > label > input[type=checkbox]").checked = true'
                 self.driver.execute_script(js)
                 return False
@@ -91,26 +91,26 @@ class Stenci:
             return False
 
     def click_agenda(self):
-        self.i.click('//*[@id="app"]/section/aside/nav/a[2]')
+        self.click('//*[@id="app"]/section/aside/nav/a[2]')
         self.select_agenda()
         return True
 
     def set_client(self, client):
-        self.i.write(
+        self.write(
             '//*[@id="app"]/section/main/div/div[3]/div/div/div/div[1]/div/input',
             client)
         self.client_click()
         return True
 
     def client_click(self):
-        self.i.click(
+        self.click(
             '//*[@id="app"]/section/main/div/div[3]/div/table/tbody/tr/td[3]', time=40)
 
-        # self.i.click('//*[@id="modal-appointment"]/div/div[2]/div[2]/div/ul/li[3]/a')
+        # self.click('//*[@id="modal-appointment"]/div/div[2]/div[2]/div/ul/li[3]/a')
         return True
 
     def select_agenda(self):
-        select_element = self.i.element('//*[@id="professional"]')
+        select_element = self.element('//*[@id="professional"]')
         select = Select(select_element)
         select.select_by_visible_text('[Todos]')
 
@@ -125,16 +125,16 @@ class Stenci:
 
         for path in paths.keys():
             time.sleep(1)
-            el = self.i.element(paths[path]).get_attribute('value')
+            el = self.element(paths[path]).get_attribute('value')
 
             values[path] = el
 
-        select = self.i.element(
+        select = self.element(
             '//*[@id="appointment-professional"]/option[2]').get_attribute('text')
         # select = Select(select)
         values['medico'] = ' '.join(select.split()[:-1])
 
-        select_element = self.i.element('//*[@id="appointment-insurance"]')
+        select_element = self.element('//*[@id="appointment-insurance"]')
         select = Select(select_element)
         values['convenio'] = select.all_selected_options[0].text
 
@@ -144,8 +144,8 @@ class Stenci:
 
     def click_registrar_atendimento(self):
         # registrar atendimento
-        self.i.click_js('//*[@id="div-register-service"]/div/a/button')
-        self.i.click_js('//*[@id="div-register-service"]/div/ul/li/a')
+        self.click_js('//*[@id="div-register-service"]/div/a/button')
+        self.click_js('//*[@id="div-register-service"]/div/ul/li/a')
 
     def finalizar_amil(self, senha):
         self.click_procedimentos()
@@ -165,35 +165,35 @@ class Stenci:
         
     
     def click_salvar_espera(self):
-        self.i.click_js("//button[text()='Salvar e colocar em espera']")
+        self.click_js("//button[text()='Salvar e colocar em espera']")
     
     def click_procedimentos(self):
-        self.i.click('//*[@id="modal-appointment"]/div/div[2]/div[2]/div/ul/li[2]/a')
+        self.click('//*[@id="modal-appointment"]/div/div[2]/div[2]/div/ul/li[2]/a')
         time.sleep(1)
     
     def set_senha(self, senha):
-        self.i.write('//*[@id="authorization-password"]', senha)
+        self.write('//*[@id="authorization-password"]', senha)
     
     def click_finalizar(self):
-        self.i.click_js('//*[@id="modal-sadt-account"]/div/div[2]/div[3]/button[1]')
+        self.click_js('//*[@id="modal-sadt-account"]/div/div[2]/div[3]/button[1]')
         
 
     def click_preencher(self):
-        self.i.click('//*[@id="modal-sadt-account"]/div/div[2]/div[2]/div/div[5]/div[6]/button')
+        self.click('//*[@id="modal-sadt-account"]/div/div[2]/div[2]/div/div[5]/div[6]/button')
     
     def registra_procedimentos(self):
-        self.i.click_js('//*[@id="div-register-service"]/div/a/button')
+        self.click_js('//*[@id="div-register-service"]/div/a/button')
     
     def selecionar_consulta(self):
         xpath = "//td[text()='Consulta em Consultório']"
-        self.i.click(xpath)
+        self.click(xpath)
     
     
     def click_lupa(self):
-        self.i.click('//*[@id="expenses"]/div/form/div/div/div/form/div/button')
+        self.click('//*[@id="expenses"]/div/form/div/div/div/form/div/button')
 
     def click_sp(self):
-        self.i.click_js("//a[text()='Guia de SP/SADT']")
+        self.click_js("//a[text()='Guia de SP/SADT']")
         
     def click_sair(self):
         path = '//button[text()="Sair"]'
@@ -210,15 +210,15 @@ class Stenci:
         self.click_registrar_atendimento()
         time.sleep(3)
 
-        self.i.element('//*[@id="referral-service-type"]', time=30)
+        self.element('//*[@id="referral-service-type"]', time=30)
 
         self.clicks_select_final()
 
         el = '//*[@id="modal-appointment"]/div/div[2]/div[3]/button[3]'
         # //*[@id="modal-appointment"]/div/div[2]/div[3]/button[3]
-        self.i.click_js(el, time=40)
+        self.click_js(el, time=40)
 
-        logging.info(self.i.element(el).get_attribute('outerHTML'))
+        logging.info(self.element(el).get_attribute('outerHTML'))
         time.sleep(2)
 
     def clicks_select_final(self):
@@ -228,16 +228,16 @@ class Stenci:
                   ]
 
         for el in clicks:
-            self.i.click(el,  time=40)
+            self.click(el,  time=40)
 
         el = '//*[@id="modal-particular-account"]/div/div[2]/div[3]/button[1]'
-        self.i.click_js(el,  time=40)
+        self.click_js(el,  time=40)
         time.sleep(2)
 
     def extrair_medicos(self):
         host = 'https://stenci.app'
         self.driver.get('https://stenci.app/clinical/professionals')
-        medicos = self.i.elements(
+        medicos = self.elements(
             '//*[@id="app"]/section/main/div/div[2]/div[2]/table/tbody/tr/td/a')
 
         all_medicos = []
@@ -251,18 +251,18 @@ class Stenci:
 
                 # time.sleep(2)
 
-                name = self.i.element(
+                name = self.element(
                     '//*[@id="company-name"]').get_attribute('value')
-                cpf = self.i.element(
+                cpf = self.element(
                     '//*[@id="company-cpf"]').get_attribute('value')
-                conselho = self.i.element(
+                conselho = self.element(
                     '//*[@id="professional-council"]').get_attribute('value')
-                uf = self.i.element(
+                uf = self.element(
                     '//*[@id="professional-council-state"]').get_attribute('value')
 
-                especialidade = self.i.element(
+                especialidade = self.element(
                     '//*[@id="professional"]/div[5]/div[5]/table/tbody/tr[2]/td[1]').text
-                name_especialidade = self.i.element(
+                name_especialidade = self.element(
                     '//*[@id="professional"]/div[5]/div[5]/table/tbody/tr[2]/td[2]').text
 
                 medico_dict = {
